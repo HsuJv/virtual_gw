dae7301c205f29b009ca8ec48fd785cbcd79818b
Jovi Hsu <jv.hsu@outlook.com>
2021-12-05 12:23:05 +0800

    swith to tun crate
    Signed-off-by: Jovi Hsu <jv.hsu@outlook.com>

--- src/common/common.rs
+++ src/common/common.rs
@@ -5,47 +5,57 @@ use tokio::io::AsyncWriteExt;
 use tokio::io::{AsyncReadExt, BufReader};
 use tokio::net::TcpStream;
 use tokio_openssl::SslStream;
-use tokio_tun::Tun;
-// use tun::{AsyncDevice, TunPacket};
+// use tokio_tun::Tun;
+use futures::{SinkExt, StreamExt};
+use tun::{AsyncDevice, TunPacket};
 
-pub async fn main_loop(tun: Tun, tunnel: BufReader<SslStream<TcpStream>>) -> AsyncReturn<()> {
-    // let mut tun = tun.into_framed();
-    let tun = BufReader::new(tun);
+// pub async fn main_loop(tun: Tun, tunnel: BufReader<SslStream<TcpStream>>) -> AsyncReturn<()> {
+pub async fn main_loop(
+    tun: AsyncDevice,
+    tunnel: BufReader<SslStream<TcpStream>>,
+) -> AsyncReturn<()> {
+    let mut tun = tun.into_framed();
+    // let tun = BufReader::new(tun);
     let mut tunnel_buf = [0u8; 1480];
-    let mut tun_buf = [0u8; 1480];
+    // let mut tun_buf = [0u8; 1480];
 
-    let (mut tun_reader, mut tun_writer) = tokio::io::split(tun);
+    // let (mut tun_reader, mut tun_writer) = tokio::io::split(tun);
     let (mut tunnel_reader, mut tunnel_writer) = tokio::io::split(tunnel);
 
     loop {
-        let tun_active = tun_reader.read(&mut tun_buf).fuse();
+        // let tun_active = tun_reader.read(&mut tun_buf).fuse();
+        let tun_active = tun.next().fuse();
         let tunnel_active = tunnel_reader.read(&mut tunnel_buf).fuse();
 
         pin_mut!(tun_active, tunnel_active);
         select! {
             res  = tun_active => {
-                match res {
-                    Ok(n) => {
-                        if 0 != n {
-                            if 0x45 == tun_buf[0] {
-                                tunnel_writer.write_all(&tun_buf[..n]).await.unwrap();
-                                debug!("Write {:?}", &tun_buf[..n]);
-                            } else {
-                                // debug!("{:?}", n);
-                            }
-                        }
-                    }
-                    Err(e) => {
-                        info!("close connection {:?}", e);
-                    }
+                // match res {
+                //     Ok(n) => {
+                //         if 0 != n {
+                //             if 0x45 == tun_buf[0] {
+                //                 debug!("Write {:x?}", &tun_buf[..n]);
+                //                 tunnel_writer.write_all(&tun_buf[..n]).await.unwrap();
+                //             } else {
+                //                 // debug!("{:?}", n);
+                //             }
+                //         }
+                //     }
+                //     Err(e) => {
+                //         info!("close connection {:?}", e);
+                //     }
+                // }
+                if let Ok(packet) = res.unwrap() {
+                    tunnel_writer.write_all(packet.get_bytes()).await.unwrap();
                 }
             },
             res  = tunnel_active => {
                 match res {
                     Ok(n) => {
                         if 0 != n {
-                            tun_writer.write_all(&tunnel_buf[0..n]).await.unwrap();
-                            debug!("Recv {:?}", &tunnel_buf[..n]);
+                            debug!("Recv {:x?}", &tunnel_buf[..n]);
+                            // tun_writer.write_all(&tunnel_buf[0..n]).await.unwrap();
+                            tun.send(TunPacket::new(tunnel_buf[0..n].to_vec())).await.unwrap();
                         } else {
                             // debug!("{}", n);
                         }
--- src/tunnel/tun.rs
+++ src/tunnel/tun.rs
@@ -1,40 +1,47 @@
 use crate::AsyncReturn;
 use log::*;
-use tokio_tun::{Tun, TunBuilder};
+use tun::AsyncDevice;
+// use tokio_tun::{Tun, TunBuilder};
 
-async fn create_tun_with_ip(ip: &str) -> AsyncReturn<Tun> {
-    let tun = TunBuilder::new()
-        .name("") // if name is empty, then it is set by kernel.
-        .tap(false) // false (default): TUN, true: TAP.
-        .mtu(1350)
-        .address(ip.parse().unwrap())
-        .netmask("255.255.255.255".parse().unwrap())
-        .packet_info(false) // false: IFF_NO_PI, default is true.
-        .up() // or set it up manually using `sudo ip link set <tun-name> up`.
-        .try_build()
-        .unwrap(); // or `.try_build_mq(queues)` for multi-queue support.
-
-    Ok(tun)
-}
-
-// async fn create_tun_with_ip(ip: &str) -> AsyncReturn<AsyncDevice> {
-//     let mut config = tun::Configuration::default();
-
-//     config
-//         .address(ip)
-//         .netmask((255, 255, 255, 255))
+// async fn create_tun_with_ip(ip: &str) -> AsyncReturn<Tun> {
+//     let tun = TunBuilder::new()
+//         .name("") // if name is empty, then it is set by kernel.
+//         .tap(false) // false (default): TUN, true: TAP.
 //         .mtu(1350)
-//         .up();
+//         .address(ip.parse().unwrap())
+//         .netmask("255.255.255.255".parse().unwrap())
+//         .packet_info(false) // false: IFF_NO_PI, default is true.
+//         .up() // or set it up manually using `sudo ip link set <tun-name> up`.
+//         .try_build()
+//         .unwrap(); // or `.try_build_mq(queues)` for multi-queue support.
 
-//     #[cfg(target_os = "linux")]
-//     config.platform(|config| {
-//         config.packet_information(false);
-//     });
-
-//     Ok(tun::create_as_async(&config).unwrap())
+//     Ok(tun)
 // }
 
-pub async fn create_tun(addr: &str) -> AsyncReturn<Tun> {
+// pub async fn create_tun(addr: &str) -> AsyncReturn<Tun> {
+//     let dev = create_tun_with_ip(addr).await?;
+//     info!("Crate tun : {}", addr);
+//     Ok(dev)
+// }
+
+async fn create_tun_with_ip(ip: &str) -> AsyncReturn<AsyncDevice> {
+    let mut config = tun::Configuration::default();
+
+    config
+        .address(ip)
+        .netmask((255, 255, 255, 255))
+        .mtu(1350)
+        .up();
+
+    #[cfg(target_os = "linux")]
+    config.platform(|config| {
+        config.packet_information(false);
+    });
+
+    Ok(tun::create_as_async(&config).unwrap())
+}
+
+pub async fn create_tun(addr: &str) -> AsyncReturn<AsyncDevice> {
     let dev = create_tun_with_ip(addr).await?;
     info!("Crate tun : {}", addr);
     Ok(dev)

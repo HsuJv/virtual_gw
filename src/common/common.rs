ef51a28cc0af5b9213e5c973f1dfd44f43adbe4d
Jovi Hsu <jv.hsu@outlook.com>
2021-12-18 11:01:16 +0800

    codes cleanup
    Signed-off-by: Jovi Hsu <jv.hsu@outlook.com>

--- src/common/common.rs
+++ src/common/common.rs
@@ -1,47 +1,26 @@
 use crate::AsyncReturn;
 use futures::{future::FutureExt, pin_mut, select};
+use futures::{SinkExt, StreamExt};
 use log::*;
 use tokio::io::AsyncWriteExt;
 use tokio::io::{AsyncReadExt, BufReader};
 use tokio::net::TcpStream;
 use tokio_openssl::SslStream;
-// use tokio_tun::Tun;
-use futures::{SinkExt, StreamExt};
 use tun::{AsyncDevice, TunPacket};
 
-// pub async fn main_loop(tun: Tun, tunnel: BufReader<SslStream<TcpStream>>) -> AsyncReturn<()> {
 pub async fn main_loop(tun: AsyncDevice, ssl: BufReader<SslStream<TcpStream>>) -> AsyncReturn<()> {
     let mut tun = tun.into_framed();
-    // let tun = BufReader::new(tun);
-    let mut ssl_buf = [0u8; 1480];
-    // let mut tun_buf = [0u8; 1480];
+    let mut ssl_buf = [0u8; 1600];
 
-    // let (mut tun_reader, mut tun_writer) = tokio::io::split(tun);
     let (mut ssl_reader, mut ssl_writer) = tokio::io::split(ssl);
 
     loop {
-        // let tun_active = tun_reader.read(&mut tun_buf).fuse();
         let tun_active = tun.next().fuse();
         let ssl_active = ssl_reader.read(&mut ssl_buf).fuse();
 
         pin_mut!(tun_active, ssl_active);
         select! {
             res  = tun_active => {
-                // match res {
-                //     Ok(n) => {
-                //         if 0 != n {
-                //             if 0x45 == tun_buf[0] {
-                //                 debug!("Write {:x?}", &tun_buf[..n]);
-                //                 ssl_writer.write_all(&tun_buf[..n]).await.unwrap();
-                //             } else {
-                //                 // debug!("{:?}", n);
-                //             }
-                //         }
-                //     }
-                //     Err(e) => {
-                //         info!("close connection {:?}", e);
-                //     }
-                // }
                 if let Ok(packet) = res.unwrap() {
                     debug!("Write {:02x?}", packet.get_bytes().len());
                     ssl_writer.write_all(packet.get_bytes()).await.unwrap();
@@ -50,9 +29,8 @@ pub async fn main_loop(tun: AsyncDevice, ssl: BufReader<SslStream<TcpStream>>) -
             res  = ssl_active => {
                 let n = res.unwrap();
                 if 0 != n {
-                    debug!("Recv {:02x?}", &ssl_buf[..n].len());
-                    // tun_writer.write_all(&ssl_buf[0..n]).await.unwrap();
-                    tun.send(TunPacket::new(ssl_buf[0..n].to_vec())).await.unwrap();
+                    debug!("Recv {:02x?}", &ssl_buf.len());
+                    tun.send(TunPacket::new(ssl_buf.to_vec())).await.unwrap();
                 } else {
                     return Ok(());
                 }
--- src/tunnel/tun.rs
+++ src/tunnel/tun.rs
@@ -1,28 +1,6 @@
 use crate::AsyncReturn;
 use log::*;
 use tun::AsyncDevice;
-// use tokio_tun::{Tun, TunBuilder};
-
-// async fn create_tun_with_ip(ip: &str) -> AsyncReturn<Tun> {
-//     let tun = TunBuilder::new()
-//         .name("") // if name is empty, then it is set by kernel.
-//         .tap(false) // false (default): TUN, true: TAP.
-//         .mtu(1350)
-//         .address(ip.parse().unwrap())
-//         .netmask("255.255.255.255".parse().unwrap())
-//         .packet_info(false) // false: IFF_NO_PI, default is true.
-//         .up() // or set it up manually using `sudo ip link set <tun-name> up`.
-//         .try_build()
-//         .unwrap(); // or `.try_build_mq(queues)` for multi-queue support.
-
-//     Ok(tun)
-// }
-
-// pub async fn create_tun(addr: &str) -> AsyncReturn<Tun> {
-//     let dev = create_tun_with_ip(addr).await?;
-//     info!("Crate tun : {}", addr);
-//     Ok(dev)
-// }
 
 async fn create_tun_with_ip(ip: &str) -> AsyncReturn<AsyncDevice> {
     let mut config = tun::Configuration::default();
